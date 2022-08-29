1bedf933f6c15160650f4d0be8b728e9153d77e1
Carlos Motiño <carlos.motino@elcatex.com>
2022-08-29 13:42:48 -0600

    first commit

---
+++ MauiApp1/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+using MauiApp1.Data;
+using ZXing.Net.Maui;
+using ZXing;
+using ZXing.Net.Maui.Readers;
+
+namespace MauiApp1;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+
+		builder.Services.AddMauiBlazorWebView();
+		#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+		
+		builder.Services.AddSingleton<WeatherForecastService>();		
+		builder.UseBarcodeReader();
+
+		return builder.Build();
+	}
+}
